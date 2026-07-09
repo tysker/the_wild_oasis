f687f92b9e902aea1ebffcaf8c4310309df8cb73
tysker <joergoertel@proton.me>
2026-07-09 11:59:36 +0200

    add logout feature

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,3 +1,4 @@
+import Logout from '../features/authentication/Logout';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
@@ -7,5 +8,9 @@ const StyledHeader = styled.header`
 `;
 
 export default function Header() {
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logout />
+    </StyledHeader>
+  );
 }
